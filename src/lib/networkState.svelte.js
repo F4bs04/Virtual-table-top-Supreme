e509241842b0d72c801a473a4ea0979bb8fc3269
BleachVTT User <player@bleachvtt.com>
2026-06-19 20:56:20 -0300

    Fix safeEquals reactive proxy comparisons causing interaction lockouts

--- src/lib/networkState.svelte.js
+++ src/lib/networkState.svelte.js
@@ -22,12 +22,8 @@ function safeEquals(a, b) {
   
   if (a instanceof Date) return b instanceof Date && a.getTime() === b.getTime();
   
-  const keysA = Object.keys(a);
   const keysB = Object.keys(b);
-  if (keysA.length !== keysB.length) return false;
-  
-  for (const key of keysA) {
-    if (!keysB.includes(key)) return false;
+  for (const key of keysB) {
     if (!safeEquals(a[key], b[key])) return false;
   }
   return true;
